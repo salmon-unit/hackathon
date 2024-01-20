d06ef562193297fd2aaf925ef7fd01901865ce35
bluePumpkin2 <134378358+bluePumpkin2@users.noreply.github.com>
2024-01-20 21:01:57 +0900

    ログイン・新規登録画面完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_20_115300) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_20_115904) do
   create_table "books", force: :cascade do |t|
     t.string "book_title"
     t.string "book_image"
