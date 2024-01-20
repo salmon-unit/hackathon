72e3ff330cf59251ef7e676367d93ad37f0b281d
riku <riku2004@gmail.com>
2024-01-20 18:33:02 +0900

    fix error:SQLite3::SQLException: table books already exists

--- db/migrate/20240120061746_create_books.rb
+++ db/migrate/20240120061746_create_books.rb
@@ -1,18 +1,18 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
-  def change
-    create_table :books do |t|
-      t.string :book_title
-      t.string :book_image
-      t.integer :user_id
-      t.integer :rate
-      t.text :comment
-      t.string :author
-      t.date :publication_date
-      t.string :genre
-      t.text :short_comment
-      t.text :favorite_sentence
+    def change
+        create_table :books, force: true do |t|
+            t.string :book_title
+            t.string :book_image
+            t.integer :user_id
+            t.integer :rate
+            t.text :comment
+            t.string :author
+            t.date :publication_date
+            t.string :genre
+            t.text :short_comment
+            t.text :favorite_sentence
 
-      t.timestamps
+            t.timestamps
+        end
     end
-  end
 end
