2092dbc549ad7b2a06ee1183a10eb4818459eb5e
bluePumpkin2 <134378358+bluePumpkin2@users.noreply.github.com>
2024-01-20 12:39:12 +0900

    土台作成

---
+++ test/system/books_test.rb
@@ -0,0 +1,59 @@
+require "application_system_test_case"
+
+class BooksTest < ApplicationSystemTestCase
+  setup do
+    @book = books(:one)
+  end
+
+  test "visiting the index" do
+    visit books_url
+    assert_selector "h1", text: "Books"
+  end
+
+  test "should create book" do
+    visit books_url
+    click_on "New book"
+
+    fill_in "Author", with: @book.author
+    fill_in "Book", with: @book.book_id
+    fill_in "Book image", with: @book.book_image
+    fill_in "Book title", with: @book.book_title
+    fill_in "Comment", with: @book.comment
+    fill_in "Favorite sentence", with: @book.favorite_sentence
+    fill_in "Genre", with: @book.genre
+    fill_in "Publication date", with: @book.publication_date
+    fill_in "Short comment", with: @book.short_comment
+    fill_in "User", with: @book.user_id
+    click_on "Create Book"
+
+    assert_text "Book was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Book" do
+    visit book_url(@book)
+    click_on "Edit this book", match: :first
+
+    fill_in "Author", with: @book.author
+    fill_in "Book", with: @book.book_id
+    fill_in "Book image", with: @book.book_image
+    fill_in "Book title", with: @book.book_title
+    fill_in "Comment", with: @book.comment
+    fill_in "Favorite sentence", with: @book.favorite_sentence
+    fill_in "Genre", with: @book.genre
+    fill_in "Publication date", with: @book.publication_date
+    fill_in "Short comment", with: @book.short_comment
+    fill_in "User", with: @book.user_id
+    click_on "Update Book"
+
+    assert_text "Book was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Book" do
+    visit book_url(@book)
+    click_on "Destroy this book", match: :first
+
+    assert_text "Book was successfully destroyed"
+  end
+end
