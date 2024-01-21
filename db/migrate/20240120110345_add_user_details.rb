9ccbe7f6d7eb2fc20dd70473f617a56137395244
riku <riku2004@gmail.com>
2024-01-21 12:38:49 +0900

    migrateファイルを編集

--- db/migrate/20240120110345_add_user_details.rb
+++
@@ -1,7 +0,0 @@
-class AddUserDetails < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :introduction, :string
-    add_column :users, :email, :string
-    add_column :users, :profile_image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_21_031312) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_20_065454) do
   create_table "books", force: :cascade do |t|
-    t.integer "book_id"
     t.string "book_title"
     t.string "book_image"
     t.integer "user_id"
+    t.integer "rate"
     t.text "comment"
     t.string "author"
     t.date "publication_date"
@@ -27,14 +27,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_21_031312) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "user_password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "introduction"
-    t.string "email"
-    t.string "profile_image"
-    t.string "user_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
